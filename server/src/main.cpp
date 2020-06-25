4db3a086676302ab2b9254d38941a6ae6844df11
IMConstant <31627442+IMConstant@users.noreply.github.com>
2020-06-25 23:12:30 +0300

    Update main.cpp

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -3,13 +3,6 @@
 #include "Server.h"
 
 
-std::ostream &operator<<(std::ostream &output, const char *str) {
-    output << std::string(str) << std::string(" saset zhopu");
-
-    return output;
-}
-
-
 int main() {
     Server server;
     server.run();
